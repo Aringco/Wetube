e38e6e78f58133da681a17e1d3ff3e1c8d707d48
Aringco <49303039+Aringco@users.noreply.github.com>
2020-03-10 00:22:25 +0900

    Day 22
    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,36 @@
 const path = require("path");
-const ExtractCss = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
         test: /\.(scss)$/,
-        use: ExtractCss.extract([
+        use: ExtractCSS.extract([
           {
             loader: "css-loader"
           },
           {
-            loader: "postcss-loader"
+            loader: "postcss-loader",
+            options: {
+              plugins() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              }
+            }
           },
           {
             loader: "sass-loader"
@@ -25,6 +39,7 @@ const config = {
       }
     ]
   },
-  output: { path: OUTPUT_DIR, filename: "[name].[format]" }
+  output: { path: OUTPUT_DIR, filename: "[name].js" },
+  plugins: [new ExtractCSS("styles.css")]
 };
 module.exports = config;
